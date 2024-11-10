d48a48ca244f0e0e5596b921f317a5dff1bf015c
Umar Karim <umarkarim178@gmail.com>
2024-11-10 12:11:48 +0500

    updates

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    category = request.form.get("category")
+    print(category)
     return render_template("index.html")
 
 if __name__ == '__main__':
