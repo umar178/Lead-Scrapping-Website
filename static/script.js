2d4e177c1e8b3ddabe3392a19fd3281b20d710e6
Umar Karim <umarkarim178@gmail.com>
2024-11-27 11:18:22 +0500

    website completed

--- static/script.js
+++ static/script.js
@@ -6,3 +6,51 @@ function toggleMenu() {
         menu.style.display = 'block';
     }
 }
+
+function showProject(){
+    var m = document.getElementById("Members");
+    var p = document.getElementById("project");
+    var s = document.getElementById("scope-of-work");
+    var f = document.getElementById("functionality");
+
+    p.style.display = "block";
+    m.style.display = "none";
+    s.style.display = "none";
+    f.style.display = "none";
+}
+
+function showMembers(){
+    var m = document.getElementById("Members");
+    var p = document.getElementById("project");
+    var s = document.getElementById("scope-of-work");
+    var f = document.getElementById("functionality");
+
+    m.style.display = "block";
+    p.style.display = "none";
+    s.style.display = "none";
+    f.style.display = "none";
+}
+
+function showScope(){
+    var m = document.getElementById("Members");
+    var p = document.getElementById("project");
+    var s = document.getElementById("scope-of-work");
+    var f = document.getElementById("functionality");
+
+    m.style.display = "none";
+    p.style.display = "none";
+    s.style.display = "block";
+    f.style.display = "none";
+}
+
+function showFunctionality(){
+    var m = document.getElementById("Members");
+    var p = document.getElementById("project");
+    var s = document.getElementById("scope-of-work");
+    var f = document.getElementById("functionality");
+
+    m.style.display = "none";
+    p.style.display = "none";
+    s.style.display = "none";
+    f.style.display = "block";
+}
